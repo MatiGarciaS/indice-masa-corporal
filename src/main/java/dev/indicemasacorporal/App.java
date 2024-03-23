94ed935bb6f6c0ec4ee0d65d6b572ef86fee6771
Matilde García Suárez <matigarcia0817@gmail.com>
2024-03-23 21:21:51 +0100

    ejercicio finalizado

--- src/main/java/dev/indicemasacorporal/App.java
+++ src/main/java/dev/indicemasacorporal/App.java
@@ -1,5 +1,7 @@
 package dev.indicemasacorporal;
-
+import java.util.Scanner;
+import dev.indicemasacorporal.models.Calcular;
+import dev.indicemasacorporal.models.Person;
 /**
  * Hello world!
  */
@@ -11,7 +13,52 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World!");
+    // }
+
+
+public static void main(String[] args) {
+
+    Scanner dataEntry = new Scanner(System.in);
+
+
+    System.out.println("Introduce tu altura en metros: ");
+    double height1 = dataEntry.nextDouble();
+
+    System.out.println("Introduce tu peso en kilogramos: ");
+    double weight1 = dataEntry.nextDouble();
+
+
+    Person person = new Person(weight1, height1);
+
+    Calcular imcCaculator = new Calcular();
+
+    String result = imcCaculator.calculateIMC(person);
+
+    double calculated = person.getWeight() / Math.pow(person.getHeight(), 2);
+    
+    System.out.println ("Tu IMC es: " + calculated  + result);
+
+
+
+
+    dataEntry.close();
 }
+
+
+/**
+ * Hello world!
+ */
+// public final class App {
+//     private App() {
+//     }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World!");
+    }
+// }
---
+++ src/main/java/dev/indicemasacorporal/models/Calcular.java
@@ -0,0 +1,40 @@
+package dev.indicemasacorporal.models;
+
+public class Calcular {
+
+    public String calculateIMC(Person person) {
+
+        double weight = person.getWeight();
+        double height = person.getHeight();
+
+        double imc = weight / (Math.pow(height, 2));
+        
+        if (imc <16) {
+            return " Delgadez severa";
+        }        
+        if (imc >= 16 && imc < 17) {
+        return" Delgadez moderada";
+        }        
+        if (imc >= 17 && imc < 18.5) {
+        return" Delgadez leve";
+        }        
+        if (imc >= 18.5 && imc < 25) {
+            return" Peso normal";
+        }        
+        if  (imc >= 25 && imc < 30) {
+            return" Sobrepeso";
+        }        
+        if (imc >= 30 && imc < 35) {
+            return" Obesidad leve";
+        }
+        if (imc >= 35 && imc < 40) {
+            return" Obesidad moderada";
+        }
+        else {
+            return" Obesidad mórbida";
+        }
+    }   
+}
+
+
+
